e0c3f70eb651343aeb11f0fa409e586c78d2e157
vladislav <staik4@yandex.ru>
2018-06-27 19:35:37 +0300

    Create calculator

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+  let valueOfInsvest =3000000;
+  let valuePerYear = 817974;
+  $('#calculate').click(function(){
+    let manyOfInvest = $('.volume-of-invest :selected').val();
+    let nameOfProjecs = $('.select-projects :selected').val();
+    let yearsOfInvest = $('.per-year-invest :selected').val();
+    console.log(yearsOfInvest, nameOfProjecs, manyOfInvest);
+  });
 
 });
