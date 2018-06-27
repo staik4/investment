a113b12d608c5c0a6c15727b3e93e96384204619
vladislav <staik4@yandex.ru>
2018-06-27 14:45:54 +0300

    New function scroll in main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  console.log(window.focus())
+
   console.log("pageYOffset", window.pageYOffset);
 
   function header(){
@@ -14,7 +16,7 @@ $(document).ready(function() {
     $('.navv').addClass('col-xl-8');
     $('.navv').removeClass('col-xl-7');
     $("#logo").css({"display":"block"});
-  };
+  };//function for design navigation bar depending on situation
 
   if (window.pageYOffset >= 100) {
     let header = $('#header');
@@ -28,20 +30,22 @@ $(document).ready(function() {
     $('.navv').removeClass('col-xl-8');
     $('.navv').addClass('col-xl-7');
     // console.log('kus');
-  }
+  }//reload page -- out nav bar on top
+
   function animateArrow(){
-    $(".arrow-top").animate({width: "70px"}, 1500, function(){
-        $(".arrow-top").animate({width: "60px"}, 1500, function(){
+    $(".arrow-top").animate({bottom: "115px"}, 700, function(){
+        $(".arrow-top").animate({bottom: "100px"}, 700, function(){
           animateArrow();
           console.log('ani');
         });
 
     });
-  }
+  }//animate arrow
+
     $(".arrow-top").mouseover(function(){
       console.log('kuku');
       animateArrow();
-    });
+    });//start animate arrow
 
     $(".arrow-top").click(function(){
       $('html, body').stop().animate({scrollTop:"0"},1500, function(){
@@ -49,7 +53,9 @@ $(document).ready(function() {
         $(".arrow-top").addClass('hide');
       });
 
-    });
+    });//click arrow top
+
+
 
   let height = $('.navigation-header').height();
   //console.log(height);
@@ -83,7 +89,7 @@ $(document).ready(function() {
           $(".slide-item[data-slide='"+nSlide+"'] .slogan").css({'opacity':1});
         },3500);
 
-        console.log('nSlide',nSlide);
+        // console.log('nSlide',nSlide);
       },6000);
     } else {
       wrapper.animate({"margin-left":'-='+slideWidth}, 2500);
@@ -116,7 +122,7 @@ $(document).ready(function() {
       let that  = $(this);
       let numOfPr = $(that).data("project");
       //console.log($(".description-project[data-prnum='"+$(that).data("project")+"']"));
-      console.log(that);
+      // console.log(that);
       $(".description-project[data-prnum='"+$(that).data("project")+"']").removeClass('hide');
       $(".description-project[data-prnum='"+$(that).data("project")+"']").fadeIn('slow', function(){
 
@@ -129,12 +135,12 @@ $(document).ready(function() {
         $(".description-project[data-prnum='"+$(that).data("project")+"'] > .btn-close-desc").on('click',function(){
           $(".description-project[data-prnum='"+$(that).data("project")+"']").fadeOut('slow', function(){
             $(this).addClass('hide');
-            console.log('hrem',$(this));
+            // console.log('hrem',$(this));
           });
         });
       });
     });
-  });
+  });//projects
 
   $('.memu-payback-item').each(function(){
     $(this).on('click', function(){
@@ -178,6 +184,7 @@ $(document).ready(function() {
 
 
 window.addEventListener('wheel', onWheel);
+window.addEventListener('scroll', scroll);
 
 function onWheel(e) {
   e = e || window.event;
@@ -188,7 +195,7 @@ function onWheel(e) {
   let logotip = document.querySelector('.logo-img');
 
   // console.log(delta, window.pageYOffset);
-  if (delta < 0 && window.pageYOffset <= 100) {
+  if (delta < 0 && window.pageYOffset <= 90) {
     logo.style.display = 'block';
     header.style.top = '';
     logotip.style.display = 'none';
@@ -216,23 +223,45 @@ function onWheel(e) {
     document.querySelector('.navv').classList.remove('col-xl-8');
     document.querySelector('.navv').classList.add('col-xl-7');
     document.querySelector('.arrow-top').classList.remove('hide');
-    //console.log(start);
-    // let timer = setInterval(function() {
-    //   // вычислить сколько времени прошло с начала анимации
-    //   let timePassed = Date.now() - start;
-    //   //console.log(timePassed);
-    //   if (timePassed >= 2000) {
-    //     clearInterval(timer); // конец через 2 секунды
-    //     return;
-    //   }
-    //
-    //   // рисует состояние анимации, соответствующее времени timePassed
-    //   draw(timePassed);
-    //   function draw(timePassed) {
-    //     header.style.top = '0';
-    //     logo.style.display = 'none';
-    //   }
-    // }, 200);
   }
 
+}
+
+function scroll() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  let header = document.querySelector('#header');
+  let logo = document.querySelector('#logo');
+  let logotip = document.querySelector('.logo-img');
+
+  // console.log(delta, window.pageYOffset);
+  if (scrollTop <= 100) {
+    logo.style.display = 'block';
+    header.style.top = '';
+    logotip.style.display = 'none';
+    document.querySelector('.navigation-header').style.padding = '25px 0 25px 0';
+    document.querySelector('.logo-tip').classList.remove('col-xl-1');
+    document.querySelector('.navv').classList.remove('col-xl-7');
+    document.querySelector('.navv').classList.add('col-xl-8');
+    document.querySelector('.arrow-top').classList.add('hide');
+    // console.log('ne rabotaet');
+
+    let height = document.querySelector('.navigation-header').clientHeight;
+    //console.log(height);
+    document.querySelector('#banner').style.padding = height + "px";
+
+  }else {
+    // console.log('rabotaet');
+    header.style.top = '0';
+    logo.style.display = 'none';
+    let start = Date.now();
+    logotip.style.display = 'block';
+    logotip.style.width = '50px';
+    logotip.style.height = '50px';
+    document.querySelector('.navigation-header').style.padding = '15px 0 15px 0';
+    document.querySelector('.logo-tip').classList.add('col-xl-1');
+    document.querySelector('.navv').classList.remove('col-xl-8');
+    document.querySelector('.navv').classList.add('col-xl-7');
+    document.querySelector('.arrow-top').classList.remove('hide');
+  }
+  console.log(scrollTop);
 }
